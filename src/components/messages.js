e923fb4fb5e6604a4afc0f32649a805642bc7740
Adam Holtzman <adam.holtzman@gmail.com>
2020-05-29 15:39:33 -0400

    fix mobile styling on message page

--- src/components/messages.js
+++ src/components/messages.js
@@ -16,35 +16,48 @@ export default () => {
   }, [])
 
   return (
-    <div>
-      <div style={{textAlign: 'right', maxWidth: '600px', margin:'2rem auto'}}>
-        <button onClick={logout} style={{padding: '1rem', border: '1px solid white', fontSize: '16px', background: 'darkred', color: 'white' }}>log out</button>
+    <Wrap>
+      <BtnWrap>
+        <button onClick={logout} style={{padding: '1rem', margin:'1rem', border: '1px solid white', fontSize: '16px', background: 'darkred', color: 'white' }}>log out</button>
         <a
-          style={{padding: '1rem', background: 'white', margin:'1rem', textDecoration:'none', color:'black'}}
+          style={{textAlign: 'center', padding: '1rem', background: 'white', margin:'1rem', textDecoration:'none', color:'black'}}
           href='https://docs.google.com/spreadsheets/d/1YQxX3FEtjNo5UtPKF2FUT9BiYrpOuV8kHTcY2ZhLMe4/edit?usp=sharing'
           target='_blank'
           rel="noopener noreferrer"
         >edit messages</a>
-        <Link style={{padding: '1rem', background: 'white', margin:'1rem 0', textDecoration:'none', color:'black'}} to="/add-message">add message</Link>
-      </div>
+        <Link style={{textAlign: 'center', padding: '1rem', background: 'white', margin:'1rem', textDecoration:'none', color:'black'}} to="/add-message">add message</Link>
+      </BtnWrap>
       <List>
       {
         data ? data.map((message, index) => {
           return  <Card key={index}>
                     <p>{message['Post']}</p>
                     <hr />
-                    <p style={{fontSize: `12px`, marginLeft: '1rem'}}>Submitted by {message['Name']} on {message['Timestamp']}</p>
+                    <p style={{fontSize: `12px`}}>Submitted by {message['Name']} on {message['Timestamp']}</p>
                   </Card>
                 }) : ''
       }
       </List>
-    </div>
+    </Wrap>
 )}
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media(max-width: 768px) {
+    flex-direction: column-reverse;
+  }
+`
 
 const List = styled.div`
   max-width: 600px;
   font-size: 20px;
   margin:auto;
+
+  hr {
+    border-color: rgba(0,0,20,.9);
+  }
 
   @media(max-width: 768px) {
     width: calc(100% -2rem);
@@ -53,9 +66,21 @@ const List = styled.div`
   }
 `
 
+const BtnWrap = styled.div`
+  text-align: right;
+  max-width: 600px;
+  margin: 2rem auto;
+
+  @media(max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
 const Card = styled.div`
   margin: 1rem auto;
-  padding: 1rem;
+  padding: 2rem;
+  line-height: 1.4;
   background-color:white;
   border-radius: 20px;
   box-shadow:
